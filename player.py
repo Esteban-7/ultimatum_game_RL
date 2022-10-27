382cc2e6023c6ac8925a8341e079dfe55c927fef
Esteban <deortegal@unal.edu.co>
2022-10-27 16:19:40 +0200

    eliminated memory

--- player.py
+++ player.py
@@ -1,84 +1,78 @@
 import numpy as np
 
 ##Defines the classes related to the different players in the ultimatum game. Two players are described, player_1 is the one to get the endowment and 
-## make an offer to the player 2. Player 2 is to accept or reject the offer by player 1. 
+## makes an offer to the player 2. Player 2 is to accept or reject the offer by player 1. 
 
 class player_1:
-    def __init__(self, epsilon, alpha_learn_rate,actions, memory = False):
-        # the actions argument an integer. It will be the endowment in the game, meaning that the player can offer any integer as an action
+    def __init__(self, epsilon, alpha_learn_rate, actions):
+        # The constructor of the class. 
+        # epsilon represents the parameter for the epsilon greedy strategy. That means, with epsilon probability the agent will take a random action. 
+        # alpha_learn_rate : represents the learning rate used to updated the qtable. 
+        #  the actions argument an integer. It will be the endowment in the game, meaning that the player can offer any integer as an action
         #between 0 and the number of actions. 
         self.epsilon = epsilon
         self.alpha = alpha_learn_rate
-        self.last_action = 0 
-        self.memory = memory
         self.actions = actions
         
-
-        #q_table: for player 1, when memory is activated, an additional row is added to the table. In normal conditions player 1 only has one state since he always does the first move.
-        # when setting the memory to true, the two total rows represent the states for the player, where row is the state where player 2 rejected 
-        if self.memory: 
-            self.q_table = np.zeros(shape=(2,self.actions+1)) 
-        else:
-            self.q_table= np.zeros(shape=(1,self.actions+1))
+        #q_table: . Given the structure of the game, player 1 only has one state since he always does the first move and it doesn´t depend on player 2's actions.
+        self.q_table= np.zeros(shape=(1,self.actions+1))
         pass
 
-    def move(self, state): #state is 0 by default given that there is no memory by default
-        #greedy action
-        #First, given the q table for the player and the previous state (always 0 if there is no memory and either 0 or 1 when there is memory,
-        # representing the action taken by player 2 in the prev. episode). In the q table, the row representing the state is used (row 0 or 1),
-        # in that row the max value is found by the function max(), the index of this value is found with the index functio.
-        # The new action is set to be the index of such action (given the structure defined, the index is equal to the offer made)
-        
-        if self.memory:
-            state = state
-        else:
-            state = 0
+    def move(self, state = 0): #state is 0 by default given that there is only state for the player 1
+        # This method defines the decision process done by player 1 following an epsilon-greedy strategy. 
+        # Takes as argument the state in which the player is (always 0) and returns an integer, representing the offer to be made to player 2.
 
+        # Greedy action
+        #First, given the q table for the player, with only one row,in that row the max value is found by the function max(), 
+        # the index of this value is found with the index function.
+        # The new action is set to be the index of such action (given the structure defined, the index is equal to the offer made).
+        
         strategy = np.random.random() #a random number is generated to be compared to the epsilon rate of greedy strategy
 
-        if strategy > self.epsilon: # if the random number generated strategy is greater than epsilon then the greedy action is to be taken
+        # if the random number generated strategy is greater than epsilon then the greedy action is to be taken. That is, the greedy action will be taken with
+        # 1 - epsilon probability. 
+        if strategy > self.epsilon: 
             index_max_q = list(self.q_table[state]).index(max(self.q_table[state])) 
             action = index_max_q
         else: #if the strategy random number generated is not greater than epsilon, the exploring action is to be made
             action =   np.random.randint(low = 0, high = self.actions +1) #The randint integer will generate a random number between 0 and the endowment which will be the offer.  
         return action
         
-    def update_qtable(self, action, reward,state):
-        #Updates the qtable for the player one according to the action taken. State is set to 0 when no memory is kept, state changes according to the player 2 response on prev episode
-        #find the element in the q table that needs to be updated
-        if self.memory:
-            state = state
-        else:
-            state = 0
-        
-        self.q_table[state][action] = self.q_table[state][action]*(1 - self.alpha) + reward*self.alpha #the new value in the table is the apha weighted reward
+    def update_qtable(self, action, reward,state = 0):
+        # Updates the qtable for the player 1 according to the action taken. 
+        # State is set to 0.
+        # Finds the element in the q table that needs to be updated according to the action taken.
+
+        self.q_table[state][action] = self.q_table[state][action]*(1 - self.alpha) + reward*self.alpha 
+        #the new value in the table is the apha weighted reward
         pass
 
 
 class player_2:
 
-    def __init__(self, epsilon,alpha_learn_rate,states, actions = 2, memory = False):
-        #States should be the endowment for the game, given that is all possible options that the player 1 can offer. 
-        #Actions will be set to 2, player 2 has the option to accept (1) or reject (0) the offer by player 1
+    def __init__(self, epsilon,alpha_learn_rate,states, actions = 2):
+        # Constructor for the player 2 class.
+        # epsilon represents the parameter for the epsilon greedy strategy. That means, with epsilon probability the agent will take a random action. 
+        # alpha_learn_rate : represents the learning rate used to updated the qtable. 
+        # States should be the endowment for the game, given that is all possible options that the player 1 can offer. 
+        # Actions will be set to 2, player 2 has the option to accept (1) or reject (0) the offer by player 1.
+
         self.epsilon = epsilon
         self.alpha = alpha_learn_rate
-        self.last_action = 0
         self.actions = actions 
         self.states = states
-        self.memory = memory
         
 
         #q_table: the q table is set to have N=states rows, representing the states that are the possible offers by player 1. Columns in the table are set to two,
         # given that the player 2 only has two options.
-        #when memory is set to True, another dimension is created for the table, given that player 2 has to include its last action 
-        if self.memory: 
-            self.q_table = np.zeros(shape=(2,self.states+1,self.actions)) 
-        else:
-            self.q_table= np.zeros(shape=(self.states+1,self.actions))
+        self.q_table= np.zeros(shape=(self.states+1,self.actions))
         pass
 
     def move(self, state,*args,**kwargs):
-        #greedy action
+        # This method defines the decision process done by player 2 following an epsilon-greedy strategy. 
+        # Takes as argument the state in which the player is (the offer made by player 1) and returns an integer, 0 if rejects, 1 if accepts.
+        
+        #Greedy action
         #Given the state, which in this case will be the offer made by the player 1, the player will decide according to an epsilon - greedy strategy
         strategy = np.random.random()
 
@@ -89,11 +83,12 @@ class player_2:
             action =   np.random.randint(low = 0, high = self.actions) #one is added given that the randint function generates an open interval        
         return action
 
-    def update_qtable(self, action, reward,state):
-        #To update the qtable for each player 
+    def update_qtable(self, action, reward, state):
+        # Updates the qtable for the player 2 according to the action taken.
 
         #find the element in the q table that needs to be updated
-        self.q_table[state][action] = self.q_table[state][action]*(1 - self.alpha) + reward*self.alpha #the new value in the table is the apha weighted reward
+        self.q_table[state][action] = self.q_table[state][action]*(1 - self.alpha) + reward*self.alpha 
+        #the new value in the table is the apha weighted reward
         
         pass
 
--- ultimatum.py
+++ ultimatum.py
@@ -1,18 +1,25 @@
 import pandas as pd
 
-
+## Defines the class for the ultimatum game. 
 
 
 class ultimatum:
     
     def __init__(self,episodes, player_1, player_2, endowment,policy):
-        
+        # The constructor of the ultimatum class.
+        # episodes is the number of episodes to be played. 
+        # Takes 2 players that must be of the class player_1 and player_2 defined in the player module. 
+        # The endowment is the amount of money that will be divided in the game. 
+        # The policy argument defines the proportion of the endowment that player 2 will ask as a minimum to accept the offer in the case that the game is set to have fixed or dynamic rules.
+
+
         self.episodes = episodes
         self.player_1 = player_1
         self.player_2 = player_2
         self.endowment = endowment
         self.policy = policy
         
+        # Empty lists for results, rewards and payoffs are created to save the results for every episode and later access to this information
         self.player_1_actions = []
         self.player_2_actions = []
         self.player_1_rewards = []
@@ -20,13 +27,18 @@ class ultimatum:
         self.player_1_payoff = []
         self.player_2_payoff = []
 
-        
         pass
 
 
     def episode(self):
+        # Defines the steps taken in each episode of the game.
+        # First player 1 makes a move, meaning that he makes an offer for player 2.
+        # Player 2 then accepts the offer or rejects it. 
+        # According to the action by player 2, the rewards are set.
+        # The q values of the q tables are updated for both players according to the actions taken. 
+        # Actions, rewards and payoffs are stored in the lists previously initialized to keep track of the game 
         
-        player_1_action = self.player_1.move(state = self.player_2.last_action) #always last action of player 2 (initialized always at 0) is passed, but if memmory is not activated, the state is not used. 
+        player_1_action = self.player_1.move() 
         player_2_action = self.player_2.move(state = player_1_action, p = self.policy)
         
         if player_2_action == 0: #if player 2 rejects the offer 
@@ -40,9 +52,6 @@ class ultimatum:
         self.player_1.update_qtable(action = player_1_action, reward = reward_player_1,state = player_2_action)
         self.player_2.update_qtable(action = player_2_action, reward = reward_player_2, state = player_1_action)
         
-        self.player_1.last_action = player_1_action
-        self.player_2.last_action = player_2_action
-        
         self.player_1_actions.append(player_1_action)
         self.player_2_actions.append(player_2_action)
         self.player_1_rewards.append(reward_player_1)
@@ -51,10 +60,12 @@ class ultimatum:
         self.player_2_payoff.append(sum(self.player_2_rewards))
         pass
 
-    def play(self, dynamic = False):
-        
-        policy_values = {2000:0.35,5000:0.45,7000:0.6,10000:0.4}
-        
+    def play(self, dynamic = False, policy_values = None):
+        # Defines the behaviour for the game, that is, the repetitions of the episodes. 
+        # If the policy is dynamic, that is, the policy for player_2 to accept (which is external) is shifting in time, the policy levels must be given as an argument.
+        # For the number of episodes, an episode will be played, if the game has a dynamic policy, the value of p will be changed according to the information given. 
+        # After all episodes are played, the attribute results is created by joining the lists of all results into a dataframe for easier comprehension.
+
         for i in range(self.episodes):
             if dynamic:
                 if i in policy_values.keys():
